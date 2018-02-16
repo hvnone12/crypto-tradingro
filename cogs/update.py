2881f6c216d484c51b5b7568f858186e21a18af6
hvnone12 <imbrea97@gmail.com>
2018-02-16 09:37:46 +0200

    2

--- cogs/update.py
+++ cogs/update.py
@@ -8,11 +8,11 @@ class Update:
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    async def on_message(self, member: discord.Member):
+    async def on_message(self, message, member: discord.Member):
 	
         update_channel = self.bot.get_channel('413954987446632449')
-
-        update = discord.utils.get(data.server.roles, name='Update')
+		update = discord.utils.get(data.server.roles, name='Update')
+		if message.content.startswith('!update'):
         msg = await self.bot.send_message(update_channel, '{}, De acum vei fi anuntat cand apar informatii noi!'
                                                            ':money_mouth::moneybag::money_with_wings:'.format(member.mention))
         await self.bot.add_roles(member, update)
