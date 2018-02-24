5bfa748f5e7a5af8ade876f9b0dc61528d7e7c4f
hvnone12 <imbrea97@gmail.com>
2018-02-24 09:36:06 +0200

    4

--- cogs/roles_management.py
+++ cogs/roles_management.py
@@ -13,7 +13,7 @@ class Roles:
     @commands.command(pass_context=True)
     async def id(self, ctx: commands.Context):
         did = ctx.message.author.id
-        await self.bot.send_message(ctx.message.author, 'Discord ID-ul tau este: {}'.format(did))
+        await self.bot.send_message(ctx.message.author, 'Discord ID-ul tau este: %s '. % did)
 
     @commands.command(pass_context=True)
     async def invitatii(self, ctx):
